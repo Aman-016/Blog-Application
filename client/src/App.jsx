aabbcc4958dc783bc11055518596221ed889de82
Aman-016 <amanmehta.am2004@gmail.com>
2026-01-31 22:50:36 +0530

    Create the Footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SingIn from "./pages/SingIn";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";    
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 export default function App() {
@@ -22,7 +23,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
 
       </Routes>
-
+          <Footer />
     </BrowserRouter>
   );
 }
