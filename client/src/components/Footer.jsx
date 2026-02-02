21aeee95981aa46b6114093d93788eac6638e51b
Aman-016 <amanmehta.am2004@gmail.com>
2026-02-02 19:32:09 +0530

    Add signout Functionlaity

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -23,11 +23,11 @@ export default function FooterCom() {
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.100jsprojects.com'
+                  href='#'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  100 JS Projects
+                  Projects
                 </Footer.Link>
                 <Footer.Link
                   href='/about'
@@ -42,7 +42,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='#'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
