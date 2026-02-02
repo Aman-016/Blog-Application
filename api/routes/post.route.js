3921515e99cf4470f7f1b90bd918dd7eba3cd7d9
Aman-016 <amanmehta.am2004@gmail.com>
2026-02-02 23:16:38 +0530

    add create a post api routes

---
+++ api/routes/post.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import { createPost } from '../controllers/post.controller.js';
+
+const router = express.Router();
+
+router.post('/create', verifyToken, createPost);
+
+// later you can uncomment these
+// router.get('/getposts', getPosts);
+// router.delete('/delete/:postId', verifyToken, deletePost);
+// router.put('/update/:postId', verifyToken, updatePost);
+
+export default router;
