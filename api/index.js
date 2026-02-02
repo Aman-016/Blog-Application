071209250332f8d3ae542394acd9d75a5fa95a87
Aman-016 <amanmehta.am2004@gmail.com>
2026-02-02 15:18:39 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ const app = express();
 
 // ✅ Parse JSON bodies (THIS FIXES req.body)
 app.use(express.json());
+app.use(cookieParser());
+
+
 
 // ✅ Routes
 app.use("/api/user", userRoutes);
